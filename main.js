112acd463e2782bec0e9ab9d1598dd968de1cd44
RobertAKARobin <robertgfthomas@gmail.com>
2017-05-29 21:20:51 -0500

    Loads env

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function(){
-	var App = (function(){
 
-		return {
-			view:  function(){
-				return [
-					m('p', 'Hello')
-				];
+	m.request({
+		method: 'GET',
+		url: './env.json',
+		background: true
+	}).then(function(ENV){
+
+		var App = (function(){
+			return {
+				view:  function(){
+					return [
+						m('ul', [
+							m('li', 'Client id: ' + ENV.client_id),
+							m('li', 'Client secret: ' + ENV.client_secret)
+						])
+					];
+				}
 			}
-		}
-	})();
+		})();
 
-	m.mount(document.getElementById('app'), App);
+		m.mount(document.getElementById('app'), App);
+
+	});
+
 });
